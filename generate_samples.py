f145270c0a51dc46ce60425f01236e9837173b1a
mkhoshle <mahzadkhoshle@gmail.com>
2021-07-13 08:41:09 -0500

    updated code for single tpu-updated tensors

--- generate_samples.py
+++ generate_samples.py
@@ -192,6 +192,7 @@ def generate_images_once(model, args, raw_text, seq=None, num=8, query_template=
         
         assert num < mbz or num % mbz == 0
         output_tokens_list = []
+        print(num // mbz)
         for tim in range(max(num // mbz, 1)):
             output_tokens_list.append(filling_sequence(model, seq.clone(), args))
         
