08e3dbc1e93c58ae765fb3bf84de5f4a6b9bd959
mkhoshle <mahzadkhoshle@gmail.com>
2021-07-13 03:14:19 -0500

    updated code for single tpu-updated tensors

--- mpu/utils.py
+++ mpu/utils.py
@@ -44,7 +44,7 @@ def split_tensor_along_last_dim(tensor, num_partitions,
     last_dim = tensor.dim() - 1
     last_dim_size = divide(tensor.size()[last_dim], num_partitions)
     # Split.
-    tensor_list = torch.split(tensor, last_dim_size, dim=last_dim, device=xm.xla_device())
+    tensor_list = torch.split(tensor, last_dim_size, dim=last_dim)
     # Note: torch.split does not create contiguous tensors by default.
     if contiguous_split_chunks:
         return tuple(chunk.contiguous() for chunk in tensor_list)
